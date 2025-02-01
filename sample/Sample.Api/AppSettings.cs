e0c8cdd2aa67ade19768da774a1d6a591aed3290
furkandeveloper <furkan.dvlp@gmail.com>
2025-02-01 19:30:48 +0300

    🔨 Add AppSettings.cs

---
+++ sample/Sample.Api/AppSettings.cs
@@ -0,0 +1,10 @@
+namespace Sample.Api;
+
+public class AppSettings
+{
+    public string? ApplicationName { get; set; }
+    
+    public int MaxUsers { get; set; }
+    
+    public string? Url { get; set; }
+}
